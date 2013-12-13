cd282bd4cb9760a927e6aa8286bef9fa03df1bc8
gjo1776 <gjo1776@gmail.com>
2013-12-13 13:58:56 -0600

    Original

---
+++ InsertProductLimits/frmInsertProductLimits.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace InsertProductLimits
+{
+    partial class frmInsertProductLimits
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing && (components != null))
+        //    {
+        //        components.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gboLogin = new System.Windows.Forms.GroupBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.txtAccountGroupName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gboLogin.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gboLogin
+            // 
+            this.gboLogin.Controls.Add(this.txtUsername);
+            this.gboLogin.Controls.Add(this.txtPassword);
+            this.gboLogin.Controls.Add(this.label2);
+            this.gboLogin.Controls.Add(this.label3);
+            this.gboLogin.Controls.Add(this.btnConnect);
+            this.gboLogin.Location = new System.Drawing.Point(12, 32);
+            this.gboLogin.Name = "gboLogin";
+            this.gboLogin.Size = new System.Drawing.Size(209, 116);
+            this.gboLogin.TabIndex = 74;
+            this.gboLogin.TabStop = false;
+            this.gboLogin.Text = "TTUS Login";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(83, 19);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(111, 20);
+            this.txtUsername.TabIndex = 30;
+            this.txtUsername.Text = "JERRYADM";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(83, 45);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(111, 20);
+            this.txtPassword.TabIndex = 33;
+            this.txtPassword.Text = "1";
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "Password";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(119, 71);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 35;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click_1);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(259, 103);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(96, 23);
+            this.btnUpdate.TabIndex = 75;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 160);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(390, 22);
+            this.statusStrip1.TabIndex = 76;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // txtAccountGroupName
+            // 
+            this.txtAccountGroupName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtAccountGroupName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAccountGroupName.Location = new System.Drawing.Point(244, 73);
+            this.txtAccountGroupName.Name = "txtAccountGroupName";
+            this.txtAccountGroupName.Size = new System.Drawing.Size(111, 20);
+            this.txtAccountGroupName.TabIndex = 39;
+            this.txtAccountGroupName.Text = "ASG_ACCT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Account Group Name";
+            // 
+            // frmInsertProductLimits
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 182);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAccountGroupName);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.gboLogin);
+            this.Name = "frmInsertProductLimits";
+            this.Text = "Insert Product Limits";
+            this.gboLogin.ResumeLayout(false);
+            this.gboLogin.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gboLogin;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox txtAccountGroupName;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ InsertProductLimits/frmInsertProductLimits.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using TTUSAPI;
+
+
+namespace InsertProductLimits
+{
+    public partial class frmInsertProductLimits : Form
+    {
+        private TTUSAPI.TTUSApi m_TTUSAPI;
+
+        private Dictionary<string, TTUSAPI.DataObjects.User> m_Users;  // User objects
+        private Dictionary<int, TTUSAPI.DataObjects.AccountGroup> m_accountGroups;  // Account groups
+        private Dictionary<int, TTUSAPI.DataObjects.Gateway> m_colGateways;  // Gateway information
+        private Dictionary<string, TTUSAPI.DataObjects.GatewayLogin> m_GatewayLogins;  // User objects
+        private Dictionary<string, TTUSAPI.DataObjects.Product> m_Products;  // User objects
+        private Dictionary<int, TTUSAPI.DataObjects.MarketProductCatalog> m_MarketProductCatalogs;  // User objects
+
+        private int piInit = 0;
+        private int piProducts = 0;
+
+        public frmInsertProductLimits()
+        {
+            InitializeComponent();
+            initTTUSAPI();
+        }
+
+        public void initTTUSAPI()
+        {
+            UpdateStatusBar("Initializing...");
+            
+            m_TTUSAPI = new TTUSAPI.TTUSApi(TTUSAPI.TTUSApi.StartupMode.Normal);  //Create an Instance of the TTUSAPI
+            //Callbacks
+            m_TTUSAPI.OnConnectivityStatusUpdate += new TTUSApi.ConnectivityStatusUpdateHandler(m_TTUSAPI_OnConnectivityStatusUpdate);
+            m_TTUSAPI.OnLoginStatusUpdate += new TTUSApi.LoginStatusHandler(m_TTUSAPI_OnLoginStatusUpdate);  //Login status update
+            m_TTUSAPI.OnInitializeComplete += new TTUSApi.InitializeCompleteHandler(m_TTUSAPI_OnInitializeComplete); //API Initialization
+            m_TTUSAPI.OnAccountGroupUpdate += new TTUSApi.AccountGroupUpdateHandler(m_TTUSAPI_OnAccountGroupUpdate); //Account group updates
+            m_TTUSAPI.OnGatewayUpdate += new TTUSApi.GatewayUpdateHandler(m_TTUSAPI_OnGatewayUpdate); //Gateway information
+            m_TTUSAPI.OnUserUpdate += new TTUSApi.UserUpdateHandler(m_TTUSAPI_OnUserUpdate);  //Callback for user downloads
+            m_TTUSAPI.OnGatewayLoginUpdate += new TTUSApi.GatewayLoginUpdateHandler(m_TTUSAPI_OnGatewayLoginUpdate);
+            m_TTUSAPI.OnProductUpdate += new TTUSApi.ProductUpdateHandler(m_TTUSAPI_OnProductUpdate);
+
+
+            btnConnect.Enabled = false;
+            btnUpdate.Enabled = false;
+        }
+
+        #region TTUS Callbacks
+
+        void m_TTUSAPI_OnConnectivityStatusUpdate(object sender, ConnectivityStatusEventArgs e)
+        {
+            UpdateStatusBar("Found a TT User Setup Server, You can now login...");
+            this.btnConnect.Enabled = true;
+        }
+
+        void m_TTUSAPI_OnProductUpdate(object sender, ProductUpdateEventArgs e)
+        {
+            // create product limit profile
+            // lookup login
+            // add product limit profile to gateway login
+
+            m_MarketProductCatalogs = e.Products;
+
+            UpdateStatusBar("Downloaded Products.");
+            piProducts = 1;
+            if (piInit == 1)
+            {
+                btnUpdate.Enabled = true;
+            }
+        }
+        
+        void m_TTUSAPI_OnGatewayLoginUpdate(object sender, GatewayLoginUpdateEventArgs e)
+        {
+            m_GatewayLogins = e.GatewayLogins;
+        }
+
+        void m_TTUSAPI_OnLoginStatusUpdate(object sender, LoginStatusEventArgs e)
+        {
+            if (e.LoginResultCode == TTUSAPI.LoginResultCode.Success)
+            {
+                UpdateStatusBar("Login was Successful");
+                Console.WriteLine("Login was Successful");
+                //We have successfully logged in, so request users and FA Servers...
+                m_TTUSAPI.Initialize();   //Initialize the API to get all of the User, Fix Adapter, Account, and MGT data
+                m_TTUSAPI.GetProducts();
+            }
+            else
+            {
+                UpdateStatusBar("Error:  Login failed");
+                Environment.Exit(0);
+            }
+        }
+
+        void m_TTUSAPI_OnInitializeComplete(object sender, TTUSAPI.InitializeCompleteEventArgs e)
+        {
+            UpdateStatusBar("Initialization Complete.");
+            piInit = 1;
+            if (piProducts == 1)
+            {
+                btnUpdate.Enabled = true;
+            }
+        }
+
+        //Callback for TTUS User Updates
+        void m_TTUSAPI_OnUserUpdate(object sender, UserUpdateEventArgs e)
+        {
+            try
+            {
+                if (e.Type == UpdateType.Download)
+                {
+                    m_Users = e.Users;
+                }
+                //Update dictionary with any user updates
+                else if (e.Type == UpdateType.Added || e.Type == UpdateType.Changed || e.Type == UpdateType.Relationship)
+                {
+                    foreach (KeyValuePair<string, TTUSAPI.DataObjects.User> userItem in e.Users)
+                    {
+                        m_Users[userItem.Key] = userItem.Value;
+                    }
+                }
+                //Remove user from dictionary
+                else if (e.Type == UpdateType.Deleted)
+                {
+                    foreach (KeyValuePair<string, TTUSAPI.DataObjects.User> userItem in e.Users)
+                    {
+                        m_Users.Remove(userItem.Key);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an exception in the OnUserUpdate callback: " + ex.Message);
+            }
+        }
+
+        void m_TTUSAPI_OnAccountGroupUpdate(object sender, AccountGroupUpdateEventArgs e)
+        {
+            try
+            {
+                // Populate dictionary with Account Groups
+                if (e.Type == UpdateType.Download)
+                    m_accountGroups = e.AccountGroups;
+                // Add Account Group
+                else if (e.Type == UpdateType.Added)
+                {
+                    foreach (KeyValuePair<int, TTUSAPI.DataObjects.AccountGroup> acctGroup in e.AccountGroups)
+                    {
+                        m_accountGroups[acctGroup.Key] = acctGroup.Value;
+                    }
+                }
+                // Update Account Group
+                else if ((e.Type == UpdateType.Changed || e.Type == UpdateType.Relationship))
+                {
+                    foreach (KeyValuePair<int, TTUSAPI.DataObjects.AccountGroup> acctGroup in e.AccountGroups)
+                    {
+                        m_accountGroups[acctGroup.Key] = acctGroup.Value;
+                    }
+                }
+                // Delete Account Group
+                else if (e.Type == UpdateType.Deleted)
+                {
+                    foreach (KeyValuePair<int, TTUSAPI.DataObjects.AccountGroup> acctGroup in e.AccountGroups)
+                    {
+                        m_accountGroups.Remove(acctGroup.Key);
+                    }
+                }
+
+                //foreach (TTUSAPI.DataObjects.AccountGroup acct in m_accountGroups.Values)
+                //{
+                //}
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an exception in the OnAccountGroupUpdate callback: " + ex.Message);
+            }
+        }
+
+        // Populate dictionary of Gateway Information
+        void m_TTUSAPI_OnGatewayUpdate(object sender, GatewayUpdateEventArgs e)
+        {
+            m_colGateways = e.Gateways;
+        }
+
+        #endregion
+
+        #region UpdateUIRegion
+
+
+
+        /// <summary>
+        /// Update the status bar and write the message to the console in a thread safe way.
+        /// </summary>
+        /// <param name="message">Message to update the status bar with.</param>
+        delegate void UpdateStatusBarCallback(string message);
+        public void UpdateStatusBar(string message)
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateStatusBarCallback statCB = new UpdateStatusBarCallback(UpdateStatusBar);
+                this.Invoke(statCB, new object[] { message });
+            }
+            else
+            {
+                // Update the status bar.
+                toolStripStatusLabel1.Text = message;
+
+                // Also write this message to the console.
+                Console.WriteLine(message);
+            }
+        }
+
+        #endregion
+
+
+        //For a given product type, return the proper ID number
+        private int getProductTypeId(string productType)
+        {
+            if (productType.ToUpper().Equals("FUTURE"))
+                return 1;
+            else if (productType.ToUpper().Equals("SPREAD"))
+                return 2;
+            else if (productType.ToUpper().Equals("OPTION"))
+                return 3;
+            else
+                return -1;
+        }
+
+        //Retrieve the ID for a given exchange name
+        private int getGatewayId(string exchange)
+        {
+            foreach (TTUSAPI.DataObjects.Gateway gw in m_colGateways.Values) //Loop through gateways to map gateway name to gateway ID
+            {
+            }
+            return -1;
+        }
+
+        #region FormEventHandlers
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+  //          TTUSAPI.DataObjects.AccountGroup acctGroup;
+
+//            if (gatewayId != -1 && productTypeId != -1 && uint.TryParse(txtMaxPosition.Text, out maxPos) && uint.TryParse(txtMaxLongShort.Text, out maxLongShort))
+ //           {
+            foreach (TTUSAPI.DataObjects.AccountGroup acctGroup in m_accountGroups.Values) // Loop through downloaded account groups
+            {
+                // Clients should type in Group name to search for
+                if (acctGroup.Name.ToUpper() == txtAccountGroupName.Text.ToUpper())
+                {
+                    TTUSAPI.DataObjects.AccountGroupProfile acctGroupProfile = new TTUSAPI.DataObjects.AccountGroupProfile(acctGroup);
+
+                    TTUSAPI.DataObjects.AccountGroupProductLimitProfile productLimit;
+                    //bool productLimitSet = false;
+                    //foreach (TTUSAPI.DataObjects.AccountGroupProductLimit prodLimit in acctGroupProfile.ProductLimits.Values) //Loop through product limits associated with the account
+                    //{
+                    //    //Replace account product limits that match ones in the update file
+                    //    if (prodLimit.GatewayID == gatewayId && prodLimit.ProductTypeID == productTypeId && prodLimit.Product.ToUpper().Equals("ES"))
+                    //    {
+                    //        productLimit = new TTUSAPI.DataObjects.AccountGroupProductLimitProfile(prodLimit); //Create copy of Product Limit
+                    //        productLimit.MaxPositionPerContract = maxPos; //Set Maximum Position
+                    //        productLimit.MaxProductLongShort = maxLongShort; // Set Maximum Long/Short Position
+                    //        acctGroupProfile.AddProductLimit(productLimit); //Add product limit to account
+                    //        productLimitSet = true;
+                    //        break;
+                    //    }
+                    //}
+                    //Create new product limits if they don't currently exist for the account
+                    //if (productLimitSet == false)
+                    //{
+                        
+                    // Loop through Markets
+                    foreach (TTUSAPI.DataObjects.MarketProductCatalog marketPC in m_MarketProductCatalogs.Values)
+                    {
+                        // Loop through GWs
+                        foreach (TTUSAPI.DataObjects.GatewayProductCatalog gwPC in marketPC.GatewayProductCatalogs.Values)
+                        {
+                            // Loop through Product Types
+                            foreach (TTUSAPI.DataObjects.ProductCatalog productTypes in gwPC.ProductCatalogs.Values)
+                            {
+                                // Do only for Futures
+                                if (productTypes.ProductType.ProductTypeID == 1)
+                                {
+                                    // Loop through Products
+                                    foreach (TTUSAPI.DataObjects.Product prod in productTypes.Products.Values)
+                                    {
+                                        productLimit = new TTUSAPI.DataObjects.AccountGroupProductLimitProfile(); //Create new Product Limit
+
+                                        //Assign values to product limit from update file object
+                                        productLimit.GatewayID = gwPC.Gateway.GatewayID;
+                                        productLimit.Product = prod.Name;
+                                        productLimit.ProductTypeID = 1;  // Insert for Futures
+                                        productLimit.MaxOutrightsOrderQty = 1;
+                                        productLimit.MaxSpreadsOrderQty = 1;
+                                        productLimit.AllowTradingOutrights = true;
+                                        productLimit.AllowTradingSpreads = true;
+
+                                        //Add product limit to account group
+                                        acctGroupProfile.AddProductLimit(productLimit);
+
+                                        ResultStatus res = m_TTUSAPI.UpdateAccountGroup(acctGroupProfile); //Send updates to TTUS Server
+                                        if (res.Result == ResultType.SentToServer) //On Success
+                                        {
+                                            UpdateStatusBar("Updated account Group; " + acctGroup.Name);
+                                        }
+                                        else if (res.Result == ResultType.ValuesUnchanged) //No Change
+                                        {
+                                            UpdateStatusBar("Account Group; " + acctGroup.Name + " unchanged");
+                                        }
+                                        else //On Failure
+                                        {
+                                            UpdateStatusBar("Failed to update account Group; " + acctGroup.Name + ", " + res.ErrorMessage);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+ 
+
+        #endregion
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            // shut down the API
+            m_TTUSAPI.Logoff();  // Logoff the TTUS Server
+            m_TTUSAPI.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        private void btnConnect_Click_1(object sender, EventArgs e)
+        {
+            m_TTUSAPI.Login(txtUsername.Text, txtPassword.Text);
+
+        }
+
+    }
+}
